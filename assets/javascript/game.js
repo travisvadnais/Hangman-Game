c0cf972cb59aff001a4842bd92f8e65e571856d8
Travis Vadnais <travisvadnais@gmail.com>
2019-01-05 18:07:54 -0500

    more cleanup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,21 @@
   let hangmanWords = ["Gregor Clegane", "Joffrey Baratheon", "Dragonstone", "Winter Is Coming", "Lord of Light", "The Night King", "King's Landing", "Valar Morghulis", "Valar Dohaeris", "Milk of the Poppy", "The Red Wedding", "Rains of Castamere", "Cersei Lannister", "Valyrian Steel", "White Walkers", "Blood of My Blood", "Dothraki Screamer", "Faceless Men", "Hand of the King", "Iron Throne", "Xaro Xhoan Daxos", "Barristan Selmy", "Astapor", "Yunkai", "Three Eyed Raven", "Widow's Wail", "Dracarys", "The King of the North", "Dragonglass", "Faith of the Seven", "The Mad King", "The Long Night", "The Doom of Valyria"]
 
   let gameWord;
-
-  //variable to clone the length of the gameWord w/ underscores
   let underscores = [];
-
-  //array for incorrect letters the player guesses
-  let guessedLetters = [];
-
-  //counter for wins/losses/guesses
+  let incorrectGuesses = [];
   let wins = 0;
   let losses = 0;
   let remainingGuesses = 8;
-
-  //array to hold the two sets of hangman picture progressions
-  let pictureSet = ["Stark", "Wall"];
-
   let gameOver = false;
+  let pictureSet = ["Stark", "Wall"];
   resetGame();
   $("#resultsDisplay").text("WARNING: This Game Is Dark and Full of Spoilers");
-
   $("#start-button").click(resetGame);
   
   function resetGame() {//Set everything back to defaults
     gameOver = false;
     remainingGuesses = 8;
-    guessedLetters = [];
+    incorrectGuesses = [];
     $("#game_area").show();
     $("#resultsDisplay").text("Good Luck!");
     $("#winsCounter").text(wins);
@@ -47,17 +37,15 @@
     updateDisplay();
   }
 
-//Check entered key to see if it's a letter.  If so, change it to lowercase and continue.  
 document.onkeydown = event => {
   if (gameOver) {
       resetGame();
   } 
   else {
     let userGuess = event.key;
-    //If statement that will only process if the key pressed was a letter
     if (event.keyCode >= 65 && event.keyCode <= 90) {
       $("#resultsDisplay").text(`"${userGuess.toUpperCase()}" Is Correct!`);
-      evaluateGuess(event.key.toLowerCase());
+      evaluateGuess(userGuess.toLowerCase());
     }
   }
 }
@@ -71,16 +59,16 @@ function evaluateGuess(letter) {
 
   //First 'If' condition analyzes the 'positions' array.  If there's nothing in there, that means the letter does not exist in the gameWord, so we'll decrement remainingGuesses. 
   if (positions.length <= 0) {
-    if (guessedLetters.indexOf(` ${letter.toUpperCase()}`) === -1) {
-        //If it's not there, we're pushing it to the guessedLetters array
-        guessedLetters.push(` ${letter.toUpperCase()}`);
+    if (incorrectGuesses.indexOf(` ${letter.toUpperCase()}`) === -1) {
+        //If it's not there, we're pushing it to the incorrectGuesses array
+        incorrectGuesses.push(` ${letter.toUpperCase()}`);
         remainingGuesses--;
         $("#resultsDisplay").text(`There are no ${letter.toUpperCase()}'s.  Try again.`);
     } else {
         $("#resultsDisplay").text(`You already guessed '${letter.toUpperCase()}'. Try again.`);
     }
   } else {
-      //Here we map thru all entries in 'positions' array and change the letter in that position in the game word to the initial user's input
+      //Here we map thru all entries in 'underscores' array and change the letter in that position in the game word to the initial user's input
       gameWordToArray.map((val, i) => {
         if (val === letter) {
           underscores[i] = letter.toUpperCase();
@@ -94,7 +82,7 @@ function updateDisplay() {
   let displayUnderscores = underscores.join("");
   $("#underscores").text(displayUnderscores);
   $("#remainingGuesses").text(remainingGuesses);
-  $("#guessedLetters").text(guessedLetters);
+  $("#incorrectGuesses").text(incorrectGuesses);
   $("#hangmanIndex").attr("src",`assets/images/${pictureSetSelection}${remainingGuesses}.PNG`);
   checkWins();
 }
@@ -104,8 +92,6 @@ function checkWins() {
     wins++;
     $("#winsCounter").text(wins);
     gameOver = true;
-
-    //After win, populate 'WINNER' message
     $("#game_area").hide();
     $("#resultsDisplay").text("You Win!  Press Any Key to Play Again!");
     if (pictureSetSelection === "Stark") {
